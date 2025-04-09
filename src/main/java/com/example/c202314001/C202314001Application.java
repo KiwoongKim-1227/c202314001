65cb198ffc476aea1b2d984564edf630a15a47f1
KiwoongKim-1227 <opkiwoong@naver.com>
2025-04-09 09:53:51 +0900

    one

---
+++ src/main/java/com/example/c202314001/C202314001Application.java
@@ -0,0 +1,13 @@
+package com.example.c202314001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C202314001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C202314001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/c202314001/C202314001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c202314001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C202314001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
